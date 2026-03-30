c92341e63a1a4f923d65bc5a9777d3af93aa5265
juan guillermo <juang961225@gmail.com>
2026-03-30 17:06:30 -0500

    feat: connect MongoDB Atlas

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import connectDB from './config/database'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+// Conectar a MongoDB
+connectDB()
 
 // Middlewares
 app.use(cors())
